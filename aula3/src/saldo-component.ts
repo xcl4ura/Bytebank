5e9faab917627c53cdaee8c4a0bdc309f5c831f7
Chagas <laura.xchagas@gmail.com>
2026-01-28 00:21:16 -0300

    refactor aula2 and create aula3

---
+++ aula3/src/saldo-component.ts
@@ -0,0 +1,15 @@
+let saldo = 4000;
+
+
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
+
+if (elementoSaldo != null) {
+  elementoSaldo.textContent = formatarMoeda(saldo);
+}
+
+if (elementoDataAcesso != null) {
+    const dataAcesso: Date = new Date();
+    elementoDataAcesso.textContent = formatarData(dataAcesso, FormatoData.DIA_SEMANA_DIA_MES_ANO);
+  };
+
